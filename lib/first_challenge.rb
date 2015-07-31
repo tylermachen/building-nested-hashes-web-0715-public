24f299a2217a95b1e27a16252c151a7216f562f3
Tyler Machen <tylermachen@gmail.com>
2015-07-30 22:20:09 -0400

    complete epic tragedy and bonus

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,10 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+    :montague => {
+     # key/value pairs here
+      },
+    :capulet => {
+      # key/value pairs here
+    }
+   }
 end
